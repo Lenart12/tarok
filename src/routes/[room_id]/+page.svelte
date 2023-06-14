ec2db5e02de8391bca90b5a31553ba5b13fe8329
lenart12 <39205323+Lenart12@users.noreply.github.com>
2023-06-14 11:45:13 +0200

    Automatic state synronization

--- src/routes/[room_id]/+page.svelte
+++ src/routes/[room_id]/+page.svelte
@@ -7,6 +7,8 @@
     export let data;
 
     export let game_state: GameState | undefined = undefined;
+    let old_game_state: GameState | undefined = undefined;
+
 
     onMount(async () => {
         if (browser) {
@@ -15,18 +17,19 @@
                 rooms.push(data.room.id);   
             localStorage.setItem('rooms', JSON.stringify(rooms));
 
-            if (!io.connected) {
-                io.on('connect', () => {
-                    console.log('WS connected, joined room', data.room.id)
-                    io.emit('tarok:join-room', data.room.id)
-                });
-            } else {
+            const on_connected = () => {
+                console.log('WS connected, joined room', data.room.id)
                 io.emit('tarok:join-room', data.room.id)
             }
-            
+
+            if (io.connected) on_connected();
+            io.on('connect', on_connected);
+            io.on('reconnect', on_connected);
+            io.on('disconnect', () => game_state = undefined)            
             io.on('tarok:new-state', new_state => {
                 console.log('WS New state', new_state)
-                game_state = new_state || {};
+                old_game_state = structuredClone(new_state)
+                game_state = new_state;
             });
         }
     })
@@ -34,11 +37,13 @@
     onDestroy(() => {
         if (browser) {
             io.emit('tarok:leave-room', data.room.id)
+            io.removeAllListeners()
         }
     })
 
     function update_state() {
-        if (game_state === undefined) return;
+        if (game_state === undefined || JSON.stringify(game_state) === JSON.stringify(old_game_state)) return;
+        old_game_state = structuredClone(game_state)
         io.emit('tarok:update-state', game_state, data.room.id);
     }
 
@@ -46,16 +51,15 @@
         if (game_state === undefined) return;
         game_state.mixer--;
         if (game_state.mixer < 0) game_state.mixer = data.room.player_names.length - 1;
-        update_state();
     }
 
     function mixer_right() {
         if (game_state === undefined) return;
         game_state.mixer++;
         if (game_state.mixer >= data.room.player_names.length) game_state.mixer = 0;
-        update_state();
     }
 
+    $: game_state !== undefined && update_state()
 </script>
 
 <h1>{data.room.title}</h1>
@@ -86,6 +90,14 @@ Mešalec
 
 <h2>Nova runda</h2>
 
+{#each data.room.player_names as player_name, i}
+    <input type="radio" bind:group={game_state.mixer} value={i} id="mixer_{i}">
+    <label for="mixer_{i}">{player_name}</label>
+{/each}
+
+
+{:else}
+Nalaganje povezave
 {/if}
 
 <h2>Štetje</h2>
